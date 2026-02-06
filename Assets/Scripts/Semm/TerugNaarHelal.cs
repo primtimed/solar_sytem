77e417cf449330c179510e4a3379f96a919b0239
SemmNuesink <117771590+SemmNuesink@users.noreply.github.com>
2026-02-06 12:45:24 +0100

    Semm- UI en terug knop

---
+++ Assets/Scripts/Semm/TerugNaarHelal.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TerugNaarHelal : MonoBehaviour
+{
+    public void Terug()
+    {
+        SceneManager.LoadScene("Pan");
+    }
+}
