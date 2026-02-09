b9aa6fc2ae37869563f4f35fe1f5e439b4064e92
SemmNuesink <117771590+SemmNuesink@users.noreply.github.com>
2026-02-09 10:49:36 +0100

    Semm- infopopup

--- Assets/Scripts/Semm/TerugNaarHelal.cs
+++ Assets/Scripts/Semm/TerugNaarHelal.cs
@@ -3,8 +3,26 @@ using UnityEngine.SceneManagement;
 
 public class TerugNaarHelal : MonoBehaviour
 {
+    public GameObject infoPanel;
+    public Animator animator;
+
     public void Terug()
     {
         SceneManager.LoadScene("Pan");
     }
+
+    public void CloseMenus()
+    {
+        if(infoPanel.active == true)
+        {
+            animator.SetBool("Tween", true);
+        }
+        else
+        {
+            animator.SetBool("Tween", false);
+        }
+        
+    }
+
+
 }
